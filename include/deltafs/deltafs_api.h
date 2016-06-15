b8075188f5eb08170639a5fb81dccf5361db6af0
Qing Zheng <zhengq@cs.cmu.edu>
2016-06-15 19:28:36 -0400

    Add gigaplus and Makefile

---
+++ include/deltafs/deltafs_api.h
@@ -0,0 +1,29 @@
+#pragma once
+
+/*
+ * Copyright (c) 2014-2016 Carnegie Mellon University.
+ *
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. See the AUTHORS file for names of contributors.
+ */
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int deltafs_open(const char* __path, int __oflags, mode_t __mode, struct stat*);
+int deltafs_creat(const char* __path, mode_t __mode);
+ssize_t deltafs_pread(int __fd, void* __buf, size_t __sz, off_t __off);
+ssize_t deltafs_read(int __fd, void* __buf, size_t __sz);
+ssize_t deltafs_pwrite(int __fd, const void* __buf, size_t __sz, off_t __off);
+ssize_t deltafs_write(int __fd, const void* __buf, size_t __sz);
+int deltafs_close(int __fd);
+
+#ifdef __cplusplus
+}
+#endif
