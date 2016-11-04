b3f9eee743757f62d4e08b92b984c2b335ebb2fc
Qing Zheng <zhengq@cs.cmu.edu>
2016-11-04 11:49:19 -0400

    add unlink to cmd toolset

---
+++ src/cmds/deltafs_unlink.cc
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015-2016 Carnegie Mellon University.
+ *
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. See the AUTHORS file for names of contributors.
+ */
+
+#include "deltafs/deltafs_api.h"
+#include "deltafs/deltafs_config.h"
+#include "pdlfs-common/pdlfs_config.h"
+
+#if defined(PDLFS_GFLAGS)
+#include <gflags/gflags.h>
+#endif
+
+#if defined(PDLFS_GLOG)
+#include <glog/logging.h>
+#endif
+
+#include <errno.h>
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+#if defined(PDLFS_GLOG)
+  FLAGS_logtostderr = true;
+#endif
+#if defined(PDLFS_GFLAGS)
+  std::string usage("Sample usage: ");
+  usage += argv[0];
+  google::SetUsageMessage(usage);
+  google::SetVersionString(PDLFS_COMMON_VERSION);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+#endif
+#if defined(PDLFS_GLOG)
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+#endif
+  for (int i = 1; i < argc; i++) {
+    int r = deltafs_unlink(argv[i]);
+    if (r != 0) {
+      fprintf(stderr, "unlink: cannot unlink file '%s': %s\n", argv[i],
+              strerror(errno));
+      return -1;
+    }
+  }
+
+  return 0;
+}
