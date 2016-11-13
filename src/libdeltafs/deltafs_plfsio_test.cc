8665d2f2e51e4ceb268c6793945c223109473824
Qing Zheng <zhengq@cs.cmu.edu>
2016-11-13 14:58:10 -0500

    add plfs io write buffer test

--- src/libdeltafs/deltafs_plfsio_test.cc
+++ src/libdeltafs/deltafs_plfsio_test.cc
@@ -7,8 +7,77 @@
  * found in the LICENSE file. See the AUTHORS file for names of contributors.
  */
 
+#include "deltafs_plfsio.h"
+
+#include "pdlfs-common/coding.h"
 #include "pdlfs-common/testharness.h"
 #include "pdlfs-common/testutil.h"
+
+#include <map>
+
+namespace pdlfs {
+namespace plfsio {
+
+class WriteBufferTest {
+ public:
+  WriteBufferTest(uint32_t seed = 301) : num_entries_(0), rnd_(seed) {}
+
+  Iterator* Flush() {
+    buffer_.Finish();
+    ASSERT_EQ(buffer_.NumEntries(), num_entries_);
+    return buffer_.NewIterator();
+  }
+
+  void Add(uint64_t seq, size_t value_size = 32) {
+    std::string key;
+    PutFixed64(&key, seq);
+    std::string value;
+    test::RandomString(&rnd_, value_size, &value);
+    kv_.insert(std::make_pair(key, value));
+    buffer_.Append(key, value);
+    num_entries_++;
+  }
+
+  void CheckFirst(Iterator* iter) {
+    iter->SeekToFirst();
+    ASSERT_TRUE(iter->Valid());
+    Slice value = iter->value();
+    ASSERT_TRUE(value == kv_.begin()->second);
+    Slice key = iter->key();
+    ASSERT_TRUE(key == kv_.begin()->first);
+  }
+
+  void CheckLast(Iterator* iter) {
+    iter->SeekToLast();
+    ASSERT_TRUE(iter->Valid());
+    Slice value = iter->value();
+    ASSERT_TRUE(value == kv_.rbegin()->second);
+    Slice key = iter->key();
+    ASSERT_TRUE(key == kv_.rbegin()->first);
+  }
+
+  std::map<std::string, std::string> kv_;
+
+  uint32_t num_entries_;
+  WriteBuffer buffer_;
+  Random rnd_;
+};
+
+TEST(WriteBufferTest, Buffer) {
+  Add(3);
+  Add(2);
+  Add(1);
+  Add(5);
+  Add(4);
+
+  Iterator* iter = Flush();
+  CheckFirst(iter);
+  CheckLast(iter);
+  delete iter;
+}
+
+}  // namespace plfsio
+}  // namespace pdlfs
 
 int main(int argc, char* argv[]) {
   return ::pdlfs::test::RunAllTests(&argc, &argv);
