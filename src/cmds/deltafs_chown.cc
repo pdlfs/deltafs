54b16052ef6502b40bc66a36974055f742a58791
Qing Zheng <zhengq@cs.cmu.edu>
2016-11-30 11:38:45 -0500

    add chown to cmd tools

---
+++ src/cmds/deltafs_chown.cc
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2015-2016 Carnegie Mellon University.
+ *
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. See the AUTHORS file for names of contributors.
+ */
+
+#include "deltafs/deltafs_api.h"
+#include "deltafs/deltafs_config.h"
+#include "pdlfs-common/pdlfs_config.h"
+
+#if defined(PDLFS_GFLAGS)
+#include <gflags/gflags.h>
+#endif
+
+#if defined(PDLFS_GLOG)
+#include <glog/logging.h>
+#endif
+
+#include <errno.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main(int argc, char* argv[]) {
+#if defined(PDLFS_GLOG)
+  FLAGS_logtostderr = true;
+#endif
+#if defined(PDLFS_GFLAGS)
+  std::string usage("Sample usage: ");
+  usage += argv[0];
+  google::SetUsageMessage(usage);
+  google::SetVersionString(PDLFS_COMMON_VERSION);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+#endif
+#if defined(PDLFS_GLOG)
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+#endif
+  const uid_t my_uid = getuid();
+  const gid_t my_gid = getgid();
+  for (int i = 1; i < argc; i++) {
+    int r = deltafs_chown(argv[i], my_uid, my_gid);
+    if (r != 0) {
+      fprintf(stderr, "chown: cannot chown file '%s': %s\n", argv[i],
+              strerror(errno));
+      return -1;
+    }
+  }
+
+  return 0;
+}
