f9391efcdf0345f11a7bf192c574a86fcd89dd51
Qing Zheng <zhengqmark@gmail.com>
2016-12-22 10:40:02 +0000

    change default cmake build type to RelWithDebInfo

--- external/pdlfs-common/cmake/cmake-options.cmake
+++ external/pdlfs-common/cmake/cmake-options.cmake
@@ -31,7 +31,7 @@ enable_testing ()
 
 # setup cache variables for ccmake
 if (NOT CMAKE_BUILD_TYPE)
-    set (CMAKE_BUILD_TYPE Release
+    set (CMAKE_BUILD_TYPE RelWithDebInfo
          CACHE STRING "Choose the type of build." FORCE)
     set_property (CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS 
                   "Debug" "Release" "RelWithDebInfo" "MinSizeRel")
