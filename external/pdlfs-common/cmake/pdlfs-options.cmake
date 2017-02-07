29c806c14ce8e74dfbda5ce8b2f9a9cd0d0904b9
Chuck Cranor <chuck@ece.cmu.edu>
2017-02-07 17:11:00 -0500

    git subrepo commit external/pdlfs-common
    subrepo:
      subdir:   "external/pdlfs-common"
      merged:   "a08d754"
    upstream:
      origin:   "git@github.com:pdlfs/pdlfs-common.git"
      branch:   "master"
      commit:   "d6b849a"
    git-subrepo:
      version:  "0.3.0"
      origin:   "???"
      commit:   "???"

--- external/pdlfs-common/cmake/pdlfs-options.cmake
+++ external/pdlfs-common/cmake/pdlfs-options.cmake
@@ -9,14 +9,15 @@
 # that pdlfs-common/cmake is on CMAKE_MODULE_PATH so we can include
 # files from that directory (e.g. xpkg-import).
 #
+include (xpkg-import)
 
 #
 # pdlfs-common config flags:
 #   -DPDLFS_PLATFORM=POSIX                 -- platform (currently only posix)
-#   -DPDLFS_TARGET_OS=Linux                -- target os name (when cross compile)
-#   -DPDLFS_TARGET_OS_VERSION=4.4.0        -- target os vers. (when cross compile)
-#   -DPDLFS_HOST_OS=Linux                  -- the result of "uname -s" on host os
-#   -DPDLFS_HOST_OS_VERSION=4.4.0          -- the result of "uname -r" on host os
+#   -DPDLFS_TARGET_OS=Linux                -- target os name (cross compile)
+#   -DPDLFS_TARGET_OS_VERSION=4.4.0        -- target os vers. (cross compile)
+#   -DPDLFS_HOST_OS=Linux                  -- "uname -s" on host os
+#   -DPDLFS_HOST_OS_VERSION=4.4.0          -- "uname -r" on host os
 #   -DPDLFS_COMMON_LIBNAME=pdlfs-common    -- name for binary lib files
 #   -DPDLFS_COMMON_DEFINES='D1;D2'         -- add -DD1/-DD2 to compile options
 #
@@ -35,6 +36,8 @@
 #     - SNAPPY_LIBRARY_DIR: optional hint for finding snappy lib
 #   -DPDLFS_VERBOSE=1                      -- set max log verbose level
 #
+# output variables:
+#    PDLFS_COMPONENT_CFG                   -- list of requested components
 #
 # note: package config files for external packages must be preinstalled in 
 #       CMAKE_INSTALL_PATH or on CMAKE_PREFIX_PATH, except as noted.
@@ -61,66 +64,77 @@ set (PDLFS_HOST_OS "${CMAKE_HOST_SYSTEM_NAME}" CACHE
      STRING "Select host os (uname -s)")
 set (PDLFS_HOST_OS_VERSION "${CMAKE_HOST_SYSTEM_VERSION}" CACHE
      STRING "Select host os version (uname -r)")
+set (PDLFS_VERBOSE "0" CACHE STRING "Max verbose level in log outputs")
+set_property (CACHE PDLFS_VERBOSE PROPERTY STRINGS "0" "1" "2" "3"
+              "4" "5" "6" "7" "8" "9" "10")
+
+# ensure correct log verbose level
+add_compile_options (-DVERBOSE=${PDLFS_VERBOSE})
+
+# library naming variables...
 set (PDLFS_COMMON_LIBNAME "pdlfs-common" CACHE
      STRING "Custom name to install pdlfs-common with")
 set (PDLFS_COMMON_DEFINES "" CACHE
      STRING "Additional defines for this version of pdlfs-common")
+
+#
+# handle third party package configuration
+#
 
 set (PDLFS_GFLAGS "OFF" CACHE
      BOOL "Use GFLAGS (libgflags-dev) for arg parsing")
 set (PDLFS_GLOG "OFF" CACHE
      BOOL "Use GLOG (libgoogle-glog-dev) for logging")
 set (PDLFS_MARGO_RPC "OFF" CACHE
-     BOOL "Compile in Margo/abt-snoozer/argobots RPC interface")
+     BOOL "Include Margo/abt-snoozer/argobots RPC interface")
 set (PDLFS_MERCURY_RPC "OFF" CACHE
-     BOOL "Compile in Mercury RPC interface")
+     BOOL "Include Mercury RPC interface")
 set (PDLFS_RADOS "OFF" CACHE
-     BOOL "Compile in RADOS object store")
+     BOOL "Include RADOS object store")
 set (PDLFS_SNAPPY "OFF" CACHE
-     BOOL "Enable SNAPPY (libsnappy-dev) for compression")
-set (PDLFS_VERBOSE "0" CACHE STRING "Max verbose level in log outputs")
-set_property (CACHE PDLFS_VERBOSE PROPERTY STRINGS "0" "1" "2" "3"
-              "4" "5" "6" "7" "8" "9" "10")
-
-#
-# ensure max log verbose level
-#
-add_compile_options (-DVERBOSE=${PDLFS_VERBOSE})
+     BOOL "Include (libsnappy-dev) for compression")
 
 #
 # now start pulling the parts in.  currently we set find_package to
 # REQUIRED (so cmake will fail if the package is missing).  we could
 # remove that and print a (more meaningful?) custom error with a
-# message FATAL_ERROR ...
+# message FATAL_ERROR ...   we also set a ${PDLFS_COMPONENT_CFG}
+# variable list for users to use.
 #
-include (xpkg-import)
+unset (PDLFS_COMPONENT_CFG)
 
 if (PDLFS_GFLAGS)
     find_package(gflags REQUIRED)
+    list (APPEND PDLFS_COMPONENT_CFG "gflags")
     message (STATUS "gflags enabled")
 endif ()
 
 if (PDLFS_GLOG)
     xdual_import (glog::glog,glog,libglog REQUIRED)
+    list (APPEND PDLFS_COMPONENT_CFG "glog::glog")
     message (STATUS "glog enabled")
 endif ()
 
 if (PDLFS_MERCURY_RPC)
     find_package(mercury CONFIG REQUIRED)
+    list (APPEND PDLFS_COMPONENT_CFG "mercury")
     message (STATUS "mercury rpc enabled")
 endif ()
 
 if (PDLFS_MARGO_RPC)
     xpkg_import_module (margo REQUIRED margo)
+    list (APPEND PDLFS_COMPONENT_CFG "margo")
     message (STATUS "margo rpc enabled")
 endif ()
 
 if (PDLFS_RADOS)
     find_package(RADOS MODULE REQUIRED)
+    list (APPEND PDLFS_COMPONENT_CFG "RADOS")
     message (STATUS "rados enabled")
 endif ()
 
 if (PDLFS_SNAPPY)
     find_package(Snappy MODULE REQUIRED)
+    list (APPEND PDLFS_COMPONENT_CFG "Snappy")
     message (STATUS "snappy enabled")
 endif ()
