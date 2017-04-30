6c3bdd61064e12a4fdb5635a4d3c115bbc8cb621
Qing Zheng <zhengq@cs.cmu.edu>
2017-04-30 17:07:44 -0400

    fix bbos env impl

--- src/libdeltafs/bbos_env.cc
+++ src/libdeltafs/bbos_env.cc
@@ -1,5 +1,4 @@
 /*
- * Copyright (c) 2011 The LevelDB Authors.
  * Copyright (c) 2015-2017 Carnegie Mellon University.
  *
  * All rights reserved.
@@ -8,347 +7,147 @@
  * found in the LICENSE file. See the AUTHORS file for names of contributors.
  */
 
-#include <dirent.h>
-#include <pthread.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <deque>
-
-#include "pdlfs-common/env.h"
-#include <bbos/bbos_api.h>
+#include "bbos_env.h"
 
 namespace pdlfs {
-inline Status IOError(const Slice& err_context, int err_number) {
-  if (err_number != ENOENT && err_number != EEXIST) {
-    return Status::IOError(err_context, strerror(err_number));
-  } else if (err_number == EEXIST) {
-    return Status::AlreadyExists(err_context);
-  } else {
-    return Status::NotFound(err_context);
+
+inline Status BbosError(const std::string& err_msg, int err_num) {
+  switch (err_num) {
+    case BB_INVALID_READ:
+      return Status::IOError(err_msg, "read past EOF");
+    case BB_ERROBJ:
+      return Status::IOError(
+          err_msg, "cannot create object");  // XXX: object already exists?
+    case BB_ENOCONTAINER:
+      return Status::NotFound(err_msg, "container object not found");
+    case BB_ENOOBJ:
+      return Status::NotFound(err_msg, "no such object");
+    default:
+      return Status::Corruption(err_msg, "bbos error");
   }
 }
 
-class BbosBufferedSequentialFile : public SequentialFile {
- private:
-  bbos_handle_t bb_handle_;
-  std::string obj_name_;
-
- public:
-  BbosBufferedSequentialFile(bbos_handle_t bb_h, std::string fname)
-    : bb_handle_(bb_h), obj_name_(fname.c_str()) {}
-
-  virtual ~BbosBufferedSequentialFile() { }
-
-  virtual Status Read(size_t n, Slice* result, char* scratch) {
-    ssize_t data_read = 0;
-    size_t total_data_read = 0;
-    do {
-      data_read = bbos_read(bb_handle_, obj_name_.c_str(), (void *)(scratch + total_data_read),
-                            total_data_read, n - total_data_read);
-      if(data_read < 0) {
-        return IOError(obj_name_, data_read);
-      }
-
-      total_data_read += data_read;
-    } while(total_data_read < n);
-
-    return Status::OK();
-  }
-
-  virtual Status Skip(uint64_t n) {
-    return Status::OK();
-  }
-};
-
-class BbosSequentialFile : public SequentialFile {
- private:
-  bbos_handle_t bb_handle_;
-  std::string obj_name_;
-
- public:
-  BbosSequentialFile(bbos_handle_t bb_h, std::string fname)
-      : bb_handle_(bb_h), obj_name_(fname.c_str()) {}
-
-  virtual ~BbosSequentialFile() { }
-
-  virtual Status Read(size_t n, Slice* result, char* scratch) {
-    ssize_t data_read = 0;
-    size_t total_data_read = 0;
-    do {
-      data_read = bbos_read(bb_handle_, obj_name_.c_str(), (void *)(scratch + total_data_read),
-                            total_data_read, n - total_data_read);
-      if(data_read < 0) {
-        return IOError(obj_name_, data_read);
-      }
-
-      total_data_read += data_read;
-    } while(total_data_read < n);
-
-    return Status::OK();
-  }
-
-  virtual Status Skip(uint64_t n) {
-    return Status::OK();
-  }
-};
-
-class BbosRandomAccessFile : public RandomAccessFile {
- private:
-  bbos_handle_t bb_handle_;
-  std::string obj_name_;
-
- public:
-  BbosRandomAccessFile(bbos_handle_t bb_h, std::string fname)
-      : bb_handle_(bb_h), obj_name_(fname.c_str()) {}
-
-  virtual ~BbosRandomAccessFile() { }
-
-  virtual Status Read(uint64_t offset, size_t n, Slice* result,
-                      char* scratch) const {
-    ssize_t data_read = 0;
-    size_t total_data_read = 0;
-    do {
-      data_read = bbos_read(bb_handle_, obj_name_.c_str(), (void *)(scratch + total_data_read + offset),
-                            total_data_read, n - total_data_read);
-      if(data_read < 0) {
-        return IOError(obj_name_, data_read);
-      }
-
-      total_data_read += data_read;
-    } while(total_data_read < n);
-
-    return Status::OK();
-  }
-};
-
-class BbosBufferedWritableFile : public WritableFile {
- private:
-  bbos_handle_t bb_handle_;
-  std::string obj_name_;
-
- public:
-  BbosBufferedWritableFile(bbos_handle_t bb_h, std::string fname)
-      : bb_handle_(bb_h), obj_name_(fname.c_str()) {}
-
-  virtual ~BbosBufferedWritableFile() { }
-
-  virtual Status Append(const Slice& data) {
-    size_t total_data_written = 0;
-    ssize_t data_written = 0;
-    do {
-      data_written = bbos_append(bb_handle_, obj_name_.c_str(),
-                                 (void *)(data.data() + total_data_written),
-                                 data.size() - total_data_written);
-      if(data_written < 0) {
-        return IOError(obj_name_, data_written);
-      }
-      total_data_written += data_written;
-    } while(total_data_written < data.size());
-    return Status::OK();
-  }
-
-  virtual Status Close() {
-    return Status::OK();
-  }
-
-  virtual Status Flush() {
-    return Status::OK();
-  }
-
-  Status SyncDirIfManifest() {
-    return Status::OK();
-  }
-
-  virtual Status Sync() {
-    return Status::OK();
-  }
-};
-
-class BbosWritableFile : public WritableFile {
- private:
-  bbos_handle_t bb_handle_;
-  std::string obj_name_;
-
- public:
-  BbosWritableFile(bbos_handle_t bb_h, std::string fname)
-      : bb_handle_(bb_h), obj_name_(fname.c_str()) {}
-
-  virtual ~BbosWritableFile() { }
-
-  virtual Status Append(const Slice& buf) {
-    size_t total_data_written = 0;
-    ssize_t data_written = 0;
-    do {
-      data_written = bbos_append(bb_handle_, obj_name_.c_str(),
-                                 (void *)(buf.data() + total_data_written),
-                                 buf.size() - total_data_written);
-      if(data_written < 0) {
-        return IOError(obj_name_, data_written);
-      }
-      total_data_written += data_written;
-    } while(total_data_written < buf.size());
-    return Status::OK();
-  }
-
-  virtual Status Close() {
-    return Status::OK();
-  }
-
-  virtual Status Flush() {
-    return Status::OK();
-  }
-
-  virtual Status SyncDirIfManifest() {
-    return Status::OK();
-  }
-
-  virtual Status Sync() {
-    return Status::OK();
-  }
-};
-
-class BbosEnv: public Env {
+// A partial Env implementation using bbos api to redirect plfsdir I/O to an
+// underlying bbos service. Not all Env operations are supported.
+class BbosEnv : public Env {
  private:
   bbos_handle_t bb_handle_;
 
  public:
-  explicit BbosEnv(const char *local, const char *server,
-                 void *vhclass, void *vhctx) {
-    if(bbos_init_ext(local, server, vhclass, vhctx, &bb_handle_) != BB_SUCCESS) {
-      abort();
-    }
-  }
-  virtual ~BbosEnv() { abort(); }
+  explicit BbosEnv(bbos_handle_t bb_handle) : bb_handle_(bb_handle) {}
+
+  virtual ~BbosEnv() { bbos_finalize(bb_handle_); }
 
   virtual Status NewSequentialFile(const Slice& fname,
                                    SequentialFile** result) {
-    int ret = bbos_mkobj(bb_handle_, fname.c_str(), READ_OPTIMIZED);
-    if(ret == BB_SUCCESS) {
-      *result = new BbosSequentialFile(bb_handle_, fname.ToString());
-      return Status::OK();
-    }
-    *result = NULL;
-    return IOError(fname, ret);
+    // XXX: do we need to check object existence and how?
+    *result = new BbosSequentialFile(fname.c_str(), bb_handle_);
+    return Status::OK();
   }
 
   virtual Status NewRandomAccessFile(const Slice& fname,
                                      RandomAccessFile** result) {
-    int ret = bbos_mkobj(bb_handle_, fname.c_str(), WRITE_OPTIMIZED);
-    if(ret == BB_SUCCESS) {
-      *result = new BbosRandomAccessFile(bb_handle_, fname.ToString());
-      return Status::OK();
-    }
-    *result = NULL;
-    return IOError(fname, ret);
+    // XXX: do we need to check object existence and how?
+    *result = new BbosRandomAccessFile(fname.c_str(), bb_handle_);
+    return Status::OK();
   }
 
   virtual Status NewWritableFile(const Slice& fname, WritableFile** result) {
-    int ret = bbos_mkobj(bb_handle_, fname.c_str(), WRITE_OPTIMIZED);
-    if(ret == BB_SUCCESS) {
-      *result = new BbosWritableFile(bb_handle_, fname.ToString());
+    int ret = bbos_mkobj(bb_handle_, fname.c_str(), WRITE_OPTIMIZED);  // FIXME
+    if (ret != BB_SUCCESS) {
+      std::string bbos_err_msg("cannot create bbos object '");
+      bbos_err_msg += fname.c_str();
+      bbos_err_msg += "'";
+      *result = NULL;
+      return BbosError(bbos_err_msg, ret);
+    } else {
+      *result = new BbosWritableFile(fname.c_str(), bb_handle_);
       return Status::OK();
     }
-    *result = NULL;
-    return IOError(fname, ret);
+  }
+
+  virtual Status DeleteFile(const Slice& fname) {
+    return Status::OK();  // Noop
+  }
+
+  virtual Status GetFileSize(const Slice& fname, uint64_t* file_size) {
+    off_t ret = bbos_get_size(bb_handle_, fname.c_str());
+    if (ret < 0) {
+      std::string bbos_err_msg("cannot get bbos object length '");
+      bbos_err_msg += fname.c_str();
+      bbos_err_msg += "'";
+      *file_size = 0;
+      return BbosError(bbos_err_msg, ret);
+    } else {
+      *file_size = static_cast<uint64_t>(ret);
+      return Status::OK();
+    }
   }
 
   virtual bool FileExists(const Slice& fname) {
-    uint64_t size;
-    Status s;
-    s = GetFileSize(fname, &size);
-    if(!s.ok()) {
+    uint64_t ignored_size;
+    Status s = GetFileSize(fname, &ignored_size);
+    if (s.ok()) {
       return true;
-    }
-    return false;
-  }
-
-  virtual Status GetChildren(const Slice& dir,
-                             std::vector<std::string>* result) = 0;
-
-  virtual Status DeleteFile(const Slice& fname) {
-    return Status::OK();
-  }
-
-  virtual Status CreateDir(const Slice& dirname) = 0;
-
-  virtual Status AttachDir(const Slice& dirname) = 0;
-
-  virtual Status DeleteDir(const Slice& dirname) = 0;
-
-  virtual Status DetachDir(const Slice& dirname) = 0;
-
-  virtual Status GetFileSize(const Slice& fname, uint64_t* file_size) {
-    Status s;
-    off_t sz = bbos_get_size(bb_handle_, fname.c_str());
-    if(sz < 0) {
-      s = IOError(fname, sz);
     } else {
-      *file_size = (uint64_t) sz;
+      return false;
     }
-    return s;
+  }
+
+  virtual Status CreateDir(const Slice& dirname) { return Status::OK(); }
+  virtual Status AttachDir(const Slice& dirname) { return Status::OK(); }
+  virtual Status DeleteDir(const Slice& dirname) { return Status::OK(); }
+  virtual Status DetachDir(const Slice& dirname) { return Status::OK(); }
+
+  virtual Status GetChildren(const Slice& dir, std::vector<std::string>*) {
+    return Status::OK();
   }
 
   virtual Status CopyFile(const Slice& src, const Slice& target) {
-    Status status;
-    ssize_t total_data_read = 0;
-    ssize_t total_data_written = 0;
-    ssize_t data_read = 0;
-    ssize_t data_written = 0;
-    /* XXXSAK: replace size to be malloc'd by PFS_CHUNK_SIZE */
-    int read_chunk_size = 8 * 1024 * 1024;
-    void *buf = (void *) malloc (read_chunk_size);
-    /* We could decide to perform a GetFileSize and then loop to read the file.
-     * But doing this the following way prevents one RPC for GetFileSize.
-     */
-    do {
-      data_read = bbos_read(bb_handle_, src.c_str(), (void *)buf,
-                            total_data_read, read_chunk_size);
-      if(data_read < 0) {
-        status = IOError(src, data_read);
-        return status;
-      }
-
-      total_data_written = 0;
-      do {
-        data_written = bbos_append(bb_handle_, target.c_str(),
-                                   (void *)((char *)buf + total_data_written),
-                                   data_read - total_data_written);
-        if(data_written < 0) {
-          status = IOError(target, data_written);
-          return status;
-        }
-        total_data_written += data_written;
-      } while(total_data_written < data_read);
-
-      total_data_read += data_read;
-    } while(data_read < read_chunk_size);
-
-    free(buf);
-
-    return Status::OK();
+    return Status::NotSupported(Slice());
   }
 
-  virtual Status RenameFile(const Slice& src, const Slice& target) = 0;
+  virtual Status RenameFile(const Slice& src, const Slice& target) {
+    return Status::NotSupported(Slice());
+  }
 
-  virtual Status LockFile(const Slice& fname, FileLock** lock) = 0;
+  virtual Status LockFile(const Slice& fname, FileLock** lock) {
+    return Status::NotSupported(Slice());
+  }
 
-  virtual Status UnlockFile(FileLock* lock) = 0;
+  virtual Status UnlockFile(FileLock* lock) {
+    return Status::NotSupported(Slice());
+  }
 
-  virtual void Schedule(void (*function)(void* arg), void* arg) = 0;
+  virtual void Schedule(void (*function)(void* arg), void* arg) {
+    Env::Default()->Schedule(function, arg);
+  }
 
-  virtual void StartThread(void (*function)(void* arg), void* arg) = 0;
+  virtual void StartThread(void (*function)(void* arg), void* arg) {
+    Env::Default()->StartThread(function, arg);
+  }
 
-  virtual Status GetTestDirectory(std::string* path) = 0;
+  virtual Status GetTestDirectory(std::string* path) {
+    return Env::Default()->GetTestDirectory(path);
+  }
 
-  virtual Status NewLogger(const std::string& fname, Logger** result) = 0;
+  virtual Status NewLogger(const std::string& fname, Logger** result) {
+    return Env::Default()->NewLogger(fname, result);
+  }
 
-  virtual uint64_t NowMicros() = 0;
+  virtual uint64_t NowMicros() { return Env::Default()->NowMicros(); }
 
-  virtual void SleepForMicroseconds(int micros) = 0;
+  virtual void SleepForMicroseconds(int micros) {
+    Env::Default()->SleepForMicroseconds(micros);
+  }
 
-  virtual Status FetchHostname(std::string* hostname) = 0;
+  virtual Status FetchHostname(std::string* hostname) {
+    return Env::Default()->FetchHostname(hostname);
+  }
 
-  virtual Status FetchHostIPAddrs(std::vector<std::string>* ips) = 0;
+  virtual Status FetchHostIPAddrs(std::vector<std::string>* ips) {
+    return Env::Default()->FetchHostIPAddrs(ips);
+  }
 };
 
 }  // namespace pdlfs
---
+++ src/libdeltafs/bbos_env.h
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2015-2017 Carnegie Mellon University.
+ *
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. See the AUTHORS file for names of contributors.
+ */
+
+#pragma once
+
+#include <bbos/bbos_api.h>
+#include "pdlfs-common/env.h"
+
+namespace pdlfs {
+
+extern Status BbosError(const std::string& err_msg, int err_num);
+
+// Thread-unsafe sequential read-only file abstraction built on top of bbos
+class BbosSequentialFile : public SequentialFile {
+ private:
+  std::string obj_name_;
+  bbos_handle_t bbos_;
+  uint64_t off_;
+
+ public:
+  BbosSequentialFile(const char* obj_name, bbos_handle_t bbos)
+      : obj_name_(obj_name), bbos_(bbos), off_(0) {}
+
+  virtual ~BbosSequentialFile() {}
+
+  virtual Status Skip(uint64_t n) {
+    off_ += n;
+    return Status::OK();
+  }
+
+  virtual Status Read(size_t n, Slice* result, char* scratch) {
+    ssize_t ret = bbos_read(bbos_, obj_name_.c_str(), scratch, off_, n);
+    if (ret < 0) {
+      std::string bbos_err_msg("cannot read from bbos object '");
+      bbos_err_msg += obj_name_;
+      bbos_err_msg += "'";
+      *result = Slice();
+      return BbosError(bbos_err_msg, ret);
+    } else {
+      off_ += ret;
+      *result = Slice(scratch, ret);
+      return Status::OK();
+    }
+  }
+};
+
+// Thread-unsafe random access file built on top of bbos
+class BbosRandomAccessFile : public RandomAccessFile {
+ private:
+  std::string obj_name_;
+  bbos_handle_t bbos_;
+
+ public:
+  BbosRandomAccessFile(const char* obj_name, bbos_handle_t bbos)
+      : obj_name_(obj_name), bbos_(bbos) {}
+
+  virtual ~BbosRandomAccessFile() {}
+
+  virtual Status Read(uint64_t offset, size_t n, Slice* result,
+                      char* scratch) const {
+    ssize_t ret = bbos_read(bbos_, obj_name_.c_str(), scratch, offset, n);
+    if (ret < 0) {
+      std::string bbos_err_msg("cannot read from bbos object '");
+      bbos_err_msg += obj_name_;
+      bbos_err_msg += "'";
+      *result = Slice();
+      return BbosError(bbos_err_msg, ret);
+    } else {
+      *result = Slice(scratch, ret);
+      return Status::OK();
+    }
+  }
+};
+
+// Thread-unsafe append-only file built on top of bbos
+class BbosWritableFile : public WritableFile {
+ private:
+  std::string obj_name_;
+  bbos_handle_t bbos_;
+
+ public:
+  BbosWritableFile(const char* obj_name, bbos_handle_t bbos)
+      : obj_name_(obj_name), bbos_(bbos) {}
+
+  virtual ~BbosWritableFile() {}
+
+  virtual Status Append(const Slice& buf) {
+    uint64_t size = buf.size();
+    const char* data = buf.data();
+    // Do partial writes
+    do {
+      ssize_t ret =
+          bbos_append(bbos_, obj_name_.c_str(), const_cast<char*>(data), size);
+      if (ret < 0) {
+        std::string bbos_err_msg("cannot write into bbos object '");
+        bbos_err_msg += obj_name_;
+        bbos_err_msg += "'";
+        return BbosError(bbos_err_msg, ret);
+      } else {
+        assert(size >= ret);
+        size -= ret;
+        data += ret;
+      }
+    } while (size > 0);
+    return Status::OK();
+  }
+
+  virtual Status Flush() {
+    return Status::OK();  // Do nothing
+  }
+
+  virtual Status Sync() {
+    return Status::OK();  // XXX: fix me when bbos implements sync
+  }
+
+  virtual Status Close() {
+    return Status::OK();  // Do nothing
+  }
+};
+
+}  // namespace pdlfs
