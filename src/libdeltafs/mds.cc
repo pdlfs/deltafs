58e396aa4d37ec857d51026096d1fa59e43522ae
Qing Zheng <zhengqmark@gmail.com>
2019-03-15 01:17:09 +0000

    update src code license

--- src/libdeltafs/mds.cc
+++ src/libdeltafs/mds.cc
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2015-2017 Carnegie Mellon University.
-*
-* All rights reserved.
-*
-* Use of this source code is governed by a BSD-style license that can be
-* found in the LICENSE file. See the AUTHORS file for names of contributors.
+ * Copyright (c) 2019 Carnegie Mellon University,
+ * Copyright (c) 2019 Triad National Security, LLC, as operator of
+ *     Los Alamos National Laboratory
+ *
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. See the AUTHORS file for names of contributors.
 */
 
 #include "mds_api.h"
