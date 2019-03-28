5d17333c71e10d7d6bd13bdc8d2998b8bde1b93f
Qing Zheng <zhengq@cs.cmu.edu>
2019-03-28 04:31:15 -0400

    clean deltafs metadata server initialization code

--- src/server/deltafs_server.cc
+++ src/server/deltafs_server.cc
@@ -9,15 +9,13 @@
  * found in the LICENSE file. See the AUTHORS file for names of contributors.
  */
 
+#include "../libdeltafs/deltafs_mds.h"
 #include "deltafs/deltafs_config.h"
 
+#include "pdlfs-common/logging.h"
 #include "pdlfs-common/pdlfs_config.h"
 #include "pdlfs-common/pdlfs_platform.h"
 #include "pdlfs-common/port.h"
-
-#include "../libdeltafs/deltafs_mds.h"
-
-#include "pdlfs-common/logging.h"
 
 #if defined(PDLFS_GFLAGS)
 #include <gflags/gflags.h>
@@ -35,7 +33,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void CheckSnappyCompression() {
+namespace {
+void CheckSnappyCompression() {
   std::string compressed;
   // See if snappy is working by attempting to compress a compressible string
   const char text[] = "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy";
@@ -46,14 +45,14 @@ static void CheckSnappyCompression() {
   }
 }
 
-static void CheckCrossCompile() {
+void CheckCrossCompile() {
   if (strcmp(PDLFS_HOST_OS, PDLFS_TARGET_OS) != 0) {
     pdlfs::Warn(__LOG_ARGS__, "Cross compile detected; code compiled for %s %s",
                 PDLFS_TARGET_OS, PDLFS_TARGET_OS_VERSION);
   }
 }
 
-static void PrintWarnings() {
+void PrintWarnings() {
 // Check optimization if CC is gcc or clang
 // This also works for icc and craycc at least on linux
 #if (defined(__GNUC__) || defined(__clang__)) && !defined(__OPTIMIZE__)
@@ -69,13 +68,13 @@ static void PrintWarnings() {
 
 // Check GFLAGS
 #ifndef PDLFS_GFLAGS
-  const char msg3[] = "Gflags is not enabled; some cmd arguments are ignored";
+  const char msg3[] = "gflags is not enabled; some cmd arguments are ignored";
   pdlfs::Warn(__LOG_ARGS__, msg3);
 #endif
 
 // Check GLOG
 #ifndef PDLFS_GLOG
-  const char msg4[] = "Glog is not enabled; will use stderr instead";
+  const char msg4[] = "glog is not enabled; will use stderr instead";
   pdlfs::Warn(__LOG_ARGS__, msg4);
 #endif
 
@@ -85,40 +84,47 @@ static void PrintWarnings() {
   pdlfs::Warn(__LOG_ARGS__, msg5);
 #endif
 }
+}  // namespace
 
 namespace pdlfs {
 extern void PrintSysInfo();
 }
 
-static pdlfs::MetadataServer* srv = NULL;
+namespace {
+pdlfs::MetadataServer* srv = NULL;
 
 #if defined(DELTAFS_MPI)
-static char procname[MPI_MAX_PROCESSOR_NAME];
-#if VERBOSE >= 1 && MPI_VERSION >= 3
-static char mpistr[MPI_MAX_LIBRARY_VERSION_STRING];
-#endif
-static int mpiver = 0;
-static int mpisubver = 0;
-static int nprocs = 1;
-static int rank = 0;
+char procname[MPI_MAX_PROCESSOR_NAME];
+#if VERBOSE >= 1
+#if MPI_VERSION >= 3
+char mpistr[MPI_MAX_LIBRARY_VERSION_STRING];
+#endif  // MPI_VERSION
+int mpiver = 0;
+int mpisubver = 0;
+#endif  // VERBOSE
+int nprocs = 1;
+int rank = 0;
 #endif
 
-static inline void MaybeInterruptServer() {
+inline void MaybeInterruptServer() {
   if (srv != NULL) {
     srv->Interrupt();
   }
 }
 
-static void HandleSignal(int signal) {
+void HandleSignal(int signal) {
   if (signal == SIGINT) {
     fprintf(stderr, "\n>>> SIGINT <<<\n");  // Send feedback to terminal
     pdlfs::Info(__LOG_ARGS__, "SIGINT received");
     MaybeInterruptServer();
   }
 }
+}  // namespace
 
 int main(int argc, char* argv[]) {
 #if defined(PDLFS_GFLAGS)
+  google::SetUsageMessage("Deltafs metadata server");
+  google::SetVersionString(PDLFS_COMMON_VERSION);
   google::ParseCommandLineFlags(&argc, &argv, true);
 #endif
 #if defined(PDLFS_GLOG)
@@ -164,13 +170,14 @@ int main(int argc, char* argv[]) {
 #else
     pdlfs::Verbose(__LOG_ARGS__, 1, "mpi.ver -> MPI %d.%d", mpiver, mpisubver);
 #endif  // MPI_VERSION
-    pdlfs::Verbose(__LOG_ARGS__, 1, "mpi.proc -> %s (hostname)", procname);
-    pdlfs::Verbose(__LOG_ARGS__, 1, "mpi.nprocs -> %d", nprocs);
+    pdlfs::Verbose(__LOG_ARGS__, 1, "mpi.name -> %s (MPI_Get_processor_name)",
+                   procname);
+    pdlfs::Verbose(__LOG_ARGS__, 1, "mpi.commsz -> %d", nprocs);
     pdlfs::Verbose(__LOG_ARGS__, 1, "mpi.rank -> %d", rank);
 #endif               // VERBOSE
-    MPI_Finalize();  // MPI no longer needed
+    MPI_Finalize();  // MPI NO LONGER NEEDED
   } else {
-    pdlfs::Error(__LOG_ARGS__, "MPI init failed");
+    pdlfs::Error(__LOG_ARGS__, "MPI_Init failed");
     abort();
   }
 #endif  // DELTAFS_MPI
