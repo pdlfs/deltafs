0b478787c821933dee814e7e6c6f3560898c4f97
Qing Zheng <zhengq@cs.cmu.edu>
2019-09-07 20:14:23 -0400

    add notes to leveldb lru cache impl [ci skip]

--- external/pdlfs-common/src/cache.cc
+++ external/pdlfs-common/src/cache.cc
@@ -18,6 +18,51 @@
 #include "pdlfs-common/lru.h"
 #include "pdlfs-common/mutexlock.h"
 
+// This LRU cache implementation is primarily designed for the leveldb
+// sub-component of the codebase. For a more general LRU cache implementation,
+// consider using the LRUCache in "pdlfs-common/lru.h" directly.
+//
+// Levedb uses caches for two purposes. It caches SSTable data blocks and
+// SSTable file handles (this includes the root index of each SSTable). Each
+// SSTable data block typically consists of 4MB of data. An underlying
+// filesystem open file descriptor is associated with each SSTable file handle.
+// Caching prevents Leveldb from repeatedly reading the same data blocks and
+// opening the same set of SSTable files. Both types of operation can be
+// time-consuming.
+//
+// Known issue:
+//
+// Currently, leveldb code performs cache insertions by first looking up a key
+// in a cache and then insertion is only performed when the key is not found in
+// the cache. While each cache operation is protected by a mutex, the leveldb
+// code uses no locking to ensure the atomicity of this lookup-and-insert
+// operation.
+//
+// Thus, if two client threads perform cache insertion at about the same time,
+// they can both lookup, find nothing, and both go do the costly operation
+// (opening an SSTable file or reading an SSTable data block) and do the cache
+// insertion. The second insertion would evict the first from the cache, leaving
+// each thread having a handle on its own instance of the same data, and with
+// only the second instance in the cache and both in memory (the first instance
+// will be deleted from memory when the corresponding client thread unrefs its
+// handle).
+//
+// We could change the cache insertion behavior to not replacing any existing
+// entries, and return the current entry to the caller. But this complicates
+// client code (since the caller will have to check special cases in which a
+// cache handle is returned that is not on the caller's data). Moreover, this
+// "no-replacing" semantic does not save clients from performing the "expensive"
+// operation (reading an SSTable data block or opening an SSTable file).
+//
+// To ensure that "expensive" operations are performed only once by one client
+// thread (this is not always necessary though), the client code will have to
+// use locks outside of the cache. Thus only the first client thread does the
+// expensive operation, and the other concurrent threads wait for the first to
+// complete and reap its result.
+//
+// Unless keeping memory consumption low is critical, having multiple client
+// threads perform the expensive operation and "compete" may lead to better
+// performance.
 namespace pdlfs {
 
 Cache::~Cache() {}
